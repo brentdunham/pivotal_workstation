a97bbedaf438547bc93f32c8b1f30b7958931447
Matthew Kocher <mkocher@pivotallabs.com>
2012-03-23 15:36:03 -0700

    fixing some foodcritic errors

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,4 +1,4 @@
-node.default[:versions]= {
+node.default['versions']= {
   "homebrew" => "448fbc9afa407de85f14c71ea53ad8f6c66134ab",
   "rvm" => "45a40eaa24c1d89268746e8de2913290afd9af42",
   "Pivotal-Preferences-RubyMine" => "d0eb393ab3d4c92ac80ce3135aa9f71e59cccbd3",
